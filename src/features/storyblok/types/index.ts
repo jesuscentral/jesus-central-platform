c6afb7c86f692ff4b703aafea614958941992857
Mees Hoogendoorn <mees.hoogendoorn@incentro.com>
2025-10-09 18:10:53 +0200

    fixed typoing issues

--- src/features/storyblok/types/index.ts
+++ src/features/storyblok/types/index.ts
@@ -1,38 +1,5 @@
 import { SbBlokData } from "@storyblok/react/rsc";
 
-// Re-export all Storyblok types with consistent naming
-export type {
-  SbPage,
-  SbCard,
-  SbButton,
-  SbBadge,
-  SbBlokHero,
-  SbDonation,
-  SbFooter,
-  SbFullGrid,
-  SbGlobal,
-  SbGrid,
-  SbImage,
-  SbImageCard,
-  SbImageHero,
-  SbInformationItem,
-  SbLink,
-  SbMap,
-  SbPersonCard,
-  SbRichText,
-  SbScripture,
-  SbScriptureReferences,
-  SbScrollingText,
-  SbSection,
-  SbSermonHighlight,
-  SbSpotifyEmbed,
-  SbStatement,
-  SbStatementScripture,
-  SbStaticGrid,
-  SbTeaser,
-  SbVideoHero,
-} from "@storyblok/types/287435740670216/storyblok-components";
-
 // Generic prop types
 export interface BlokComponentProps<T extends SbBlokData = SbBlokData> {
   blok: T;
