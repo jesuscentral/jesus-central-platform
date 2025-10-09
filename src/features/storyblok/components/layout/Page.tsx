c6efa04c868a7a141e8f2c51c5790aebfa4490df
Mees Hoogendoorn <mees.hoogendoorn@incentro.com>
2025-10-09 18:07:51 +0200

    fixed Page typing

--- src/features/storyblok/components/layout/Page.tsx
+++ src/features/storyblok/components/layout/Page.tsx
@@ -7,14 +7,16 @@ import React from "react";
 import { SbPage } from "@storyblok/types/287435740670216/storyblok-components";
 
 interface PageProps {
-  blok: SbPage;
+  blok: SbPage & {
+    body: SbBlokData[];
+  };
 }
 
 const Page: React.FunctionComponent<PageProps> = ({ blok }) => {
   return (
     <div {...storyblokEditable(blok as SbBlokData)}>
-      <main {...storyblokEditable(blok as SbBlokData)}>
-        {blok.body?.map((nestedBlok) => (
+      <main>
+        {blok.body?.map((nestedBlok: SbBlokData) => (
           <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
       </main>
