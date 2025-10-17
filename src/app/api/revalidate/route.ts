37983f6d65a15e542418762a941df2e823ddc426
Mees Hoogendoorn <mees.hoogendoorn@incentro.com>
2025-10-17 10:53:02 +0200

    fixed revalidatePath features

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,10 @@
 import { revalidatePath } from "next/cache";
 
 export const GET = async (request: Request) => {
-  await revalidatePath("/[...slug]", "page");
-  await revalidatePath("/activiteiten/[slug]", "page");
+  await Promise.all([
+    revalidatePath("/", "page"),
+    revalidatePath("/[...slug]", "page"),
+    revalidatePath("/activiteiten/[slug]", "page"),
+  ]);
   return new Response("Revalidated", { status: 200 });
 };
